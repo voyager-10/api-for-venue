44ae6df0c5b4c14ed4fd4a0fd4f13d2bbf6b8866
Vivek Chanpuriya <chanpuriyavivek@hotmail.com>
2024-08-29 15:42:55 +0530

    cors updated

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path')
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname)));
